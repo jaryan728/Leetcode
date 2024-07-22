d6ef47cbe2e88146e02db4966cad640778b069c1
Aryan Jain <162011995+jaryan728@users.noreply.github.com>
2024-07-22 22:46:18 +0530

    Time: 6 ms (18.05%) | Memory: 10.7 MB (12.59%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,11 +1,24 @@
 class Solution {
 public:
+    void find(int i, vector<int>nums,     vector<vector<int>>&ans,vector<int>temp)
+    {
+        if(i==nums.size())
+        {
+            ans.push_back(nums);
+            return;
+        }
+        for(int j=i;j<nums.size();j++)
+        {
+            swap(nums[j],nums[i]);
+            find(i+1,nums,ans,temp);
+
+              swap(nums[j],nums[i]);
+        }   
+    }
     vector<vector<int>> permute(vector<int>& nums) {
      vector<vector<int>>ans;
-     sort(nums.begin(),nums.end());
-    do{
-        ans.push_back(nums);
-    }while(next_permutation(nums.begin(),nums.end()));
-    
+     vector<int>temp;
+
+    find(0,nums,ans,temp);
     return ans;
 }};
