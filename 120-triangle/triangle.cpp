6ea0388236dab1964625ca7141f4bcdc7e6791d7
Aryan Jain <162011995+jaryan728@users.noreply.github.com>
2024-08-10 16:12:39 +0530

    Time: 3 ms (83.12%) | Memory: 11.8 MB (20.06%) - LeetSync

---
+++ 120-triangle/triangle.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int solve(int i,int j,vector<vector<int>>& triangle,vector<vector<int>>& dp)
+    {
+        if(i==triangle.size()-1)
+        {
+            return triangle[i][j];
+        }
+        if(dp[i][j]!=-1)return dp[i][j];
+        if(i>=triangle.size()||j>=triangle[i].size())return 1e9;
+        int u,d;
+        u=triangle[i][j]+solve(i+1,j,triangle,dp);
+        d=triangle[i][j]+solve(i+1,j+1,triangle,dp);
+        return dp[i][j]=min(u,d);
+
+
+    }
+    int minimumTotal(vector<vector<int>>& triangle) {
+    int m=triangle.size();
+    
+    vector<vector<int>> dp(m,vector<int>(m,-1));
+    return solve(0,0,triangle,dp);
+    }
+};
