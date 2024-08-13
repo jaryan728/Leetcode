4d691507fdf482eca7fe7c8234efebc9b8162699
Aryan Jain <162011995+jaryan728@users.noreply.github.com>
2024-08-13 11:17:32 +0530

    Time: 104 ms (5.01%) | Memory: 40.1 MB (5.12%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
-    void find(int i,     vector<vector<int>>&ans,vector<int>c,int t,int s,vector<int>temp)
+    void find(int i,vector<int>temp,vector<int>& c,int target, vector<vector<int>>&ans)
     {
-        if(s>t)return;
-        if(i==c.size())
-        {
-            if(s==t)
+             if(target==0)
+            {
             ans.push_back(temp);
-            return ;
+            
+            return;
         }
-        s+=c[i];
+        if(i>=c.size()||target<0)return;
+        
+      
+       
         temp.push_back(c[i]);
-        find(i+1,ans,c,t,s,temp);
-        s-=c[i];
+        find(i+1,temp,c,target-c[i],ans);
         temp.pop_back();
         while(i+1<c.size()&&c[i]==c[i+1])i++;
-        find(i+1,ans,c,t,s,temp);
+        find(i+1,temp,c,target,ans);
+
+        
     }
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-     vector<vector<int>>ans;
-     vector<int>temp;
-     sort(candidates.begin(),candidates.end());
-     find(0,ans,candidates,target,0,temp);
-     return ans;
+    vector<vector<int>> combinationSum2(vector<int>& c, int target) {  
+      vector<vector<int>>ans;
+      vector<int>temp;
+      sort(c.begin(),c.end());
+      find(0,temp,c,target,ans);
+      return ans;
     }
 };
