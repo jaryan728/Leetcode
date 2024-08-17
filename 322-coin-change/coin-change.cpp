c13b9b29c4e70ff1b8d5b65dc11ec5427623a956
Aryan Jain <162011995+jaryan728@users.noreply.github.com>
2024-08-17 12:25:37 +0530

    Time: 37 ms (55.99%) | Memory: 55.5 MB (7.79%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -19,12 +19,13 @@ public:
     // }
     int coinChange(vector<int>& coins, int amount) {
     vector<vector<int>>dp(coins.size(),vector<int>(amount+1,0));
+    vector<int>curr(amount+1,0),prev(amount+1,0);
     if(amount==0)return 0;
     // int ans=solve(coins.size()-1,coins,amount,dp);
     // if(ans==1e9)return -1;
     for(int i=0;i<=amount;i++)
-    {if(i%coins[0]==0)dp[0][i]=i/coins[0];
-    else dp[0][i]=1e9;
+    {if(i%coins[0]==0)prev[i]=i/coins[0];
+    else prev[i]=1e9;
     }
     for(int i=1;i<coins.size();i++)
     {
@@ -33,13 +34,14 @@ public:
              int tk=1e9;
         if(j>=coins[i])
         {
-            tk=1+dp[i][j-coins[i]];
+            tk=1+curr[j-coins[i]];
         }
-        int nt=dp[i-1][j];
-         dp[i][j]=min(tk,nt);
+        int nt=prev[j];
+         curr[j]=min(tk,nt);
         }
+        prev=curr;
     }
-    int ans= dp[coins.size()-1][amount];
+    int ans= prev[amount];
     if(ans==1e9)return -1;
     return ans;
     }
